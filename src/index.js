bd0c7858507df7f628aabd53e45147d689a0f0e1
justin-bao <justin.bao91@gmail.com>
2020-12-08 23:32:48 -0800

    add tasks dummy data

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './css/style.css';
 import App from './App.js';
 
+let tasks = [
+  {name: "go to farmer's market & go on a picnic"},
+  {name: "buy some pens from muji"},
+  {name: "decorate room"}
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={tasks}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
