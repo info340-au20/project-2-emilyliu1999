1e7d239857f12db548a26e26cb8af1a567c85d08
Emily Liu <liuemily@uw.edu>
2020-12-09 15:49:56 -0800

    add firebase config stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+//FirebaseUI config
+const uiConfig = {
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      requireDisplayName: true
+    },
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID //Google login
+  ],
+  //page won't show account chooser
+  credentialHelper: 'none',
+  //use popup instead of redirect for external sign-up methods -- Google
+  signInFlow: 'popup',
+  callbacks: {
+    //Avoid redirects after sign-in
+    signInSuccessWithAuthResult: () => false,
+  },
+};
+
+function App(props) {
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((firebaseUser) => {
+      if(firebaseUser) {
+        console.log( firebaseUser.displayName + ", you are logged in!")
+        setUser(firebaseUser)
+      } else {
+        console.log("see you later!")
+        setUser(null)
+      }
+    })
+  })
+
+  //allow user to log out
+  const handleSignOut = () => {
+    setErrorMessage(null);
+    firebase.auth().signOut()
+  }
+
+  let content = null; //content to render
+
+  if(!user) { //if logged out, show signup form
+    content = (
+      <div className="login-page">
+        <h1>Welcome Back</h1>
+        <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      </div>
+    );
+  } else {
+    content = (
+      <div>
+        <WelcomeHeader user={user}>
+          {user &&
+            <button className="btn btn-warning" onClick={handleSignOut}>
+              Log Out {user.displayName}
+            </button>
+          }
+        </WelcomeHeader>
+      </div>
+    )
+  }
+}
+
+
+
 
 export default App;
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css';
 import App from './App.js';
-import firebase from 'firebase/app';
+
+import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/database';
+
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCYTipq7O1wJm8bc4RxxfdiMkr3iuI_Wrw",
+    authDomain: "flora-fauna-74097.firebaseapp.com",
+    projectId: "flora-fauna-74097",
+    storageBucket: "flora-fauna-74097.appspot.com",
+    messagingSenderId: "958808924564",
+    appId: "1:958808924564:web:d38870d3dc56cf8b4b6f81",
+    measurementId: "G-CEJXREG6P6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 let tasks = [
   {id: 1, name: "go to farmer's market & go on a picnic", complete: false},
