f598de0597f2928269cbaa1b1aca50aa2df4dcd7
Paul Pham <intersect@orion.local>
2020-12-10 16:10:20 -0800

    attempt to fix firebase

--- src/index.js
+++ src/index.js
@@ -20,6 +20,22 @@ let completed = [];
 //   {name: "decorate room"}
 // ];
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCYTipq7O1wJm8bc4RxxfdiMkr3iuI_Wrw",
+  authDomain: "flora-fauna-74097.firebaseapp.com",
+  projectId: "flora-fauna-74097",
+  storageBucket: "flora-fauna-74097.appspot.com",
+  messagingSenderId: "958808924564",
+  appId: "1:958808924564:web:d38870d3dc56cf8b4b6f81",
+  measurementId: "G-CEJXREG6P6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App tasks={tasks} completed={completed}/>
