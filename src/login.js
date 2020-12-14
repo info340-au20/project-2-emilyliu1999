42e3dbc1a4e3c5f402362980a0d12fd5fc8e564c
Emily Liu <liuemily@uw.edu>
2020-12-14 11:02:08 -0800

    add js file for login

---
+++ src/login.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'css/style.css'; //import css file!
+
+
+export default App;
