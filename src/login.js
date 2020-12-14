f1188b0b43a89af28125021ec7cb2eec1866031c
justin-bao <justin.bao91@gmail.com>
2020-12-14 10:58:53 -0800

    add other routes

---
+++ src/login.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'css/style.css'; //import css file!
+
+
+
+export default App;
